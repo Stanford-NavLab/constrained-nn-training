5cfd73272e5b97aa5a9c96df693165ee6a5f8fdd
Adam Dai <adamdai97@gmail.com>
2021-05-06 13:49:53 -0700

    collision check loss with cvxpy layers

--- util/constrained_zonotope.py
+++ util/constrained_zonotope.py
@@ -11,7 +11,7 @@ class ConstrainedZonotope(object):
     __array_priority__ = 1000 # prioritize class mul over numpy array mul
 
     ### Constructors
-    def __init__(self, center, generators, constraint_A, constraint_b):
+    def __init__(self, center, generators, constraint_A=None, constraint_b=None):
         """ Constructor
 
         Args:
@@ -26,8 +26,23 @@ class ConstrainedZonotope(object):
         self.order = generators.shape[1]
 
     def __str__(self):
-        return "center:\n {0} \n generators:\n {1} \n constraint A:\n {2} \
-            \n constraint b:\n {3}".format(self.c, self.G, self.A, self.b)
+        np.set_printoptions(precision=3)
+        ind = '\t'
+        c_str = ind + str(self.c).replace('\n','\n' + ind)
+        G_str = ind + str(self.G).replace('\n','\n' + ind)
+        if self.A is not None:
+            A_str = ind + str(self.A).replace('\n','\n' + ind)
+        else:
+            A_str = ind + str(self.A)
+        if self.b is not None:
+            b_str = ind + str(self.b).replace('\n','\n' + ind)
+        else:
+            b_str = ind + str(self.b)
+        print_str = 'center:\n' + c_str + '\ngenerators:\n' + G_str + \
+                    '\nconstraint A:\n' + A_str + '\nconstraint b:\n' + b_str
+        return print_str
+        # return "center:\n {0} \ngenerators:\n {1} \nconstraint A:\n {2} \
+        #     \nconstraint b:\n {3}".format(self.c, self.G, self.A, self.b)
 
     ### Operations
     def __add__(self, other):
@@ -62,6 +77,22 @@ class ConstrainedZonotope(object):
             G = self.G @ other
         return ConstrainedZonotope(c,G,self.A,self.b) 
 
+    def intersect(self, other):
+        """ Intersection """
+        c = self.c
+        G = np.hstack((self.G, np.zeros((self.dim, other.order))))
+        # no constraints case
+        if self.A is None and other.A is None:
+            A = np.hstack((self.G, -other.G))
+            b = other.c - self.c
+        else:
+            q1 = self.A.shape[0]; q2 = other.A.shape[0] 
+            A = np.block([[self.A, np.zeros((q1, other.order))],
+                          [np.zeros((q2, self.order)), other.A],
+                          [self.G, -other.G]])
+            b = np.vstack((self.b, other.b, other.c - self.c))
+        return ConstrainedZonotope(c,G,A,b) 
+ 
     ### Representations
     def hrep(self):
         pass
